fb021172378574cb8fb8552a4d245727987a657b
dhess <dan.hess@kippbayarea.org>
2019-07-16 21:51:31 -0700

    Refactor into class based module

--- main.py
+++ main.py
@@ -3,42 +3,37 @@ import json
 import pandas as pd
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--csv", help="file name of csv to parse")
-    parser.add_argument("--model", help="django model name")
-    parser.add_argument("--json", help="file name of json to output")
-    return parser.parse_args()
+class Seed:
+    def __init__(self, csv=None, model=None, json=None):
+        args = self._get_args()
+        csv = csv or args.csv
+        df = pd.read_csv(csv)
+        self.records = df.to_dict("records")
+        self.model = model or args.model
+        self.json = json or args.json
 
+    def _get_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--csv", help="file name of csv to parse")
+        parser.add_argument("--model", help="django model name")
+        parser.add_argument("--json", help="file name of json to output")
+        return parser.parse_args()
 
-def item_schema(record, index, model):
-    item = {"model": model, "pk": index, "fields": record}
-    return item
+    def _item_schema(self, index, record):
+        item = {"model": self.model, "pk": index, "fields": record}
+        return item
 
+    def _parse_csv(self):
+        self.data = []
+        for index, record in enumerate(self.records):
+            item = self._item_schema(index, record)
+            self.data.append(item)
 
-def csv_to_json(csv, model):
-    df = pd.read_csv(csv)
-    records = df.to_dict("records")
-    json_list = []
-    for index, record in enumerate(records):
-        item = item_schema(record, index, model)
-        json_list.append(item)
-    return json_list
-
-
-def write_seed_file(data, json_filename):
-    with open(json_filename, "w") as f:
-        json.dump(data, f, indent=4)
-
-
-def main():
-    try:
-        args = get_args()
-        json_list = csv_to_json(args.csv, args.model)
-        write_seed_file(json_list, args.json)
-    except Exception as e:
-        print(e)
+    def to_json(self):
+        self._parse_csv()
+        with open(self.json, "w") as f:
+            json.dump(self.data, f, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    Seed().to_json()
